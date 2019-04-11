1e3be8b25fa20200191ad5ca114c545fd3283b90
BxyGeek <Bxy_xiaoyu@163.com>
2019-04-11 18:12:10 +0800

    "This is project init   commit"

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -6,7 +6,7 @@ package com.baizhi;
 public class App {
     public static void main(String[] args) {
 
-
+        System.out.println("commit");
         System.out.println("Hello World!");
     }
 }
