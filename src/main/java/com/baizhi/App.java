852b2d73de5f6ae27cb0747dbc653721d09c0c4d
BxyGeek <Bxy_xiaoyu@163.com>
2019-04-12 09:46:32 +0800

    添加了一些空格

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -6,6 +6,7 @@ package com.baizhi;
 public class App {
     public static void main(String[] args) {
 
+
         System.out.println("commit");
         System.out.println("Hello World!");
         System.out.println("Welcome To My Git");
