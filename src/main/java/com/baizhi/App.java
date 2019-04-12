75bce634d6c6df33d9c09308b534c64e7141a1a2
BxyGeek <Bxy_xiaoyu@163.com>
2019-04-12 09:49:26 +0800

    添加了一些空格

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -9,6 +9,8 @@ public class App {
 
         System.out.println("commit");
         System.out.println("Hello World!");
+
+
         System.out.println("Welcome To My Git");
     }
 }
---
+++ src/main/java/com/baizhi/User.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import java.io.Serializable;
+
+/**
+ * @Author: buxy
+ * @Date: 2019/4/12  9:47
+ */
+public class User implements Serializable {
+    private String id;
+
+    public User(String id) {
+        this.id = id;
+    }
+}
